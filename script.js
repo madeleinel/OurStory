9f9791be58b5e42660c6e65cbbd98291a0fad1db
madeleinel <madeleinelinder@hotmail.com>
2017-03-21 14:32:34 +0000

    Started added functionality to the True/False buttons

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 // >> For each fact > Depending on whether the user picked the correct or incorrect option, a different window will appear
 // >> Ie for 4 facts, we need 8 different pop-up windows
 
-// When click on button: display pop-up window
-$(".btn").on("click", function() {
+// When click on one of the True/False buttons (class "choice"): display pop-up window
+$(".choice").on("click", function() {
   console.log("clicked"); // This line is to check that the button click is being registered by the browser
 
+  // When click on any of the True/False buttons: Display the pop-up window (id "popup")
+  // $("#popup").css({"display": "block;"});
 });
 
 // When click on the correct option: Display "success" text
