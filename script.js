ed6ec2853e4b4594382e469300eb4d94e2eb56fa
madeleinel <madeleinelinder@hotmail.com>
2017-03-22 14:34:42 +0000

    changed structure of modal elements

--- script.js
+++ script.js
@@ -4,19 +4,9 @@
 // >> Ie for 4 facts, we need 8 different pop-up windows
 
 // When click on one of the True/False buttons (class "choice"): display pop-up window
-$(".choice").on("click", function() {
-  console.log("clicked"); // This line is to check that the button click is being registered by the browser
+$("#firstCorrectChoice").on("click", function(e) {
+  console.log("You chose True which was correct"); // This line is to check that the button click is being registered by the browser
 
   // When click on any of the True/False buttons: Display the pop-up window (id "popup")
-  $("#popup").
-});
 
-// When click on the correct option: Display "success" text
-$(".correct").on("click", function() {
-  console.log("correct!");
-});
-
-// When click on the incorrect option: Display "failure" text
-$(".incorrect").on("click", function() {
-  console.log("incorrect!");
 });
