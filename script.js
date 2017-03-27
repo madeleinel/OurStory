dd9b5101a9e3bcfbb83aa3a6df7a3b7cfe3f3042
madeleinel <madeleinelinder@hotmail.com>
2017-03-27 17:49:18 +0100

    updated the True/False button functionality within the Facts section: the content of the popup windows is now within one modal element, with separate Success/Failure messages and additional information being called depending on the user's choice (rather than having 8 different popup windows as before

--- script.js
+++ script.js
@@ -1,59 +1,50 @@
 // Add functionality to the Facts True/False buttons
 // >> When clicking on each button, a pop-up window will appear on the screen
-// For each fact > Depending on whether the user picked the correct or incorrect option, a different window will appear
-// >> Ie for 4 facts, we need 8 different pop-up windows
+// The user will be told whether they chose correctly or not, and additional information about the fact
 
 // Only run the following code when everything else on the page has loaded:
 $(document).ready(function showPopups() {
-  // When click on the correct button for the first fact:
-  $("#btn-first-correct").click(function(e) {
-    // Display the pop-up window for: correct answer on the first "fact":
-    showWindow1C();
-  });
-  // And set up the same function for all 8 buttons in the same way:
-  $("#btn-first-incorrect").click(function(e) { showWindow1I(); });
-  $("#btn-second-correct").click(function(e) { showWindow2C(); });
-  $("#btn-second-incorrect").click(function(e) { showWindow2I(); });
-  $("#btn-third-correct").click(function(e) { showWindow3C(); });
-  $("#btn-third-incorrect").click(function(e) { showWindow3I(); });
-  $("#btn-fourth-correct").click(function(e) { showWindow4C(); });
-  $("#btn-fourth-incorrect").click(function(e) { showWindow4I(); });
 
-  // Clicking on this button will call the function that closes the pop-up windows
+  // When click on any of the True/False buttons: display the pop-up window
+  $(".choice").click(function() {
+    console.log("clicked a button");
+    $(".web_dialog").show();
+  })
+  // Clicking on the Close button will call hide the pop-up windows
   $(".btn-close").click(function(e) {
-    hideWindow();
+    $(".web_dialog").hide();
   });
+
+  // Clicking on the correct button will display a Success message
+  $(".correct").click(function(e) {
+    console.log("clicked the correct button");
+    $(".title_correct").show();
+    $(".title_incorrect").hide();
+  })
+  // Clicking on the wrong button will display a Failure message
+  $(".incorrect").click(function(e) {
+    console.log("clicked the incorrect button");
+    $(".title_incorrect").show();
+    $(".title_correct").hide();
+  })
+
+  // Clicking on a true/false button for each fact will:
+  // display more information about that specific fact,
+  // and hide the additional information about the other facts
+  $("#btn-first").click(function() {
+    $(".fact_popup").hide();
+    $("#first_fact").show();
+  })
+  $("#btn-second").click(function() {
+    $(".fact_popup").hide();
+    $("#second_fact").show();
+  })
+  $("#btn-third").click(function() {
+    $(".fact_popup").hide();
+    $("#third").show();
+  })
+  $("#btn-fourth").click(function() {
+    $(".fact_popup").hide();
+    $("#fourth").show();
+  })
 });
-
-// These functions will open the pop-up windows
-// They are being called when the user clicks on the corresponding button (see on-click functions above)
-
-// Display the pop-up window for the correct answer on the first "fact":
-function showWindow1C() { $("#popup-first-correct").show(); }
-// Display the pop-up window for the incorrect answer on the first "fact":
-function showWindow1I() { $("#popup-first-incorrect").show(); }
-// And set up the same functions for the other 3 "facts" in the same way:
-function showWindow2C() { $("#popup-second-correct").show(); }
-function showWindow2I() { $("#popup-second-incorrect").show(); }
-function showWindow3C() { $("#popup-third-correct").show(); }
-function showWindow3I() { $("#popup-third-incorrect").show(); }
-function showWindow4C() { $("#popup-fourth-correct").show(); }
-function showWindow4I() { $("#popup-fourth-incorrect").show(); }
-
-// This function will close the pop-up windows
-// It is linked to the closing button in each pop-up window: clicking one of these buttons will close all pop-up windows
-function hideWindow() {
-  $("#popup-first-correct").hide();
-  $("#popup-first-incorrect").hide();
-  $("#popup-second-correct").hide();
-  $("#popup-second-incorrect").hide();
-  $("#popup-third-correct").hide();
-  $("#popup-third-incorrect").hide();
-  $("#popup-fourth-correct").hide();
-  $("#popup-fourth-incorrect").hide();
-}
-
-// Use later on?
-// function showWindow() {
-//   console.log(this.document.activeElement.id);
-// }
